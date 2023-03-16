e1ac47d144af6789d9fd230cec3274c9718115ca
khalidaadkd <khalidabubakardawaki@gmail.com>
2023-03-15 18:17:22 -0700

    task101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -18,10 +18,9 @@ int count_word(char *s)
 	{
 		if (s[c] == ' ')
 			flag = 0;
-		else if
-			(flag == 0);
+		else if (flag == 0)
 		{
-flag = 1;
+			flag = 1;
 			w++;
 		}
 	}
@@ -60,9 +59,8 @@ char **strtow(char *str)
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
-
 				while (start < end)
-				*tmp++ = str[start++];
+					*tmp++ = str[start++];
 				*tmp = '\0';
 				matrix[k] = tmp - c;
 				k++;
